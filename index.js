83d9d777626c0c4c8c367437a9bc5c73c4517707
TPpomy <satapon041142@gmail.com>
2022-01-14 01:04:30 +0700

    RTC 14-01-2565

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const io = require("socket.io")(server, {
 
 app.use(cors());
 
-const PORT = "https://webrtc-reactjs.herokuapp.com/";
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Running is good");
